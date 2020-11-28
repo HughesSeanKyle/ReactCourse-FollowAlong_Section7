535485e99edbfa58e4ba198219d4b4fc370f8991
HughesSeanKyle <khughessean@yahoo.com>
2020-11-28 08:24:20 +0200

    V93 - Axios vs Fetch - Added Axios Lib

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'; //3
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
@@ -24,4 +25,7 @@ export default App;
         However, if properties are passed to a component authored by the programmer the method can be called/named anything. 
 
 //2 - This function was created so that we can somehow communicate to the App component what the search result was. This is done so that the App component can then communicate to the "picture render" component to display the searched pictures. 
+
+//3 - In this case it is useful for App.js to have axios as it now has access to the term state in the the SearcBar component. This will then make the request to the unsplash API to retrieve the data. 
+    By convention imports from 3rd party packages are usually put above component imports created y author/engineer. 
 */
