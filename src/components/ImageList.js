03e04cd31aeb78589cb72dd19632bea8cb6d0ec2
HughesSeanKyle <khughessean@yahoo.com>
2020-11-29 11:08:25 +0200

    V101 - Rendering Lists of Components

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Imagelist = (props) => {
-    console.log(props.images);
+    const images = props.images.map(image => {
+        return <img src={image.urls.regular} />
+    });
 
-    return <div>ImageList</div>;
+    return <div>{images}</div>;
 };
 
 export default Imagelist;
