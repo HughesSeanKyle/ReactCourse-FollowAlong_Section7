0b345280a903bc658c7e13ce1cf7e3a01451b42f
HughesSeanKyle <khughessean@yahoo.com>
2020-11-29 10:46:36 +0200

    V99 - Rendering Lists

---
+++ src/components/ImageList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Imagelist = (props) => {
+    console.log(props.images);
+
+    return <div>ImageList</div>;
+};
+
+export default Imagelist;
+
+/*
+When building a component: 
+
+First create a functional component with a simple name (perhaps name of component)
+Then hook the component up to the parent to see if that component will render. 
+If the component needs info from then parent create props and pass down from parent to component 
+If component needs state then change component from functional to class based component. 
+    If NO state needed then leave as functional
+When happy with component then extract logic from parent into child.
+*/
